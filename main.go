2f66125e7d12582748eb3c63f64d3e389a29719d
Pramod Kumar <itspramod19@gmail.com>
2026-08-30 17:17:45 +0530

    first module from yt tuts

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run(":8080")
+}
+
+/*
+
+gin-rest-api/
+├── cmd/
+│   └── server/
+│       └── main.go
+│
+├── internal/
+│   ├── handler/
+│   ├── service/
+│   ├── repository/
+│   ├── model/
+│   └── routes/
+│
+├── middleware/
+│
+├── go.mod
+└── go.sum
